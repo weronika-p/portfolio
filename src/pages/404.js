60ee9286aa35a66ac271a46fa9853edbb9370775
Weronika <weronika.pawlak1991@gmail.com>
2021-10-11 13:24:44 +0200

    feat(pages): Add about and 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ErrorPage() {
+    return (
+        <div>
+            <h2>404</h2>
+            <p>Sorry that page doesn't exist</p>
+        </div>
+    )
+}
