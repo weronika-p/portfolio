8bf6f81b13172e0e22fbb88072aee07d82a88455
Weronika <weronika.pawlak1991@gmail.com>
2021-10-12 09:32:08 +0200

    feat(graphql): Adding siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Weronika P. portfolio',
+    description: 'Portfolio',
+    copyright: 'This website is copyright 2021 Weronika P.'
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
 import * as styles from '../styles/home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
+  const { title, description } = data.site.siteMetadata
   return (
     <Layout>
       <section className={styles.header}>
@@ -13,7 +15,19 @@ export default function Home() {
           <Link className={styles.btn} to='/projects'>My portfolio projects</Link>
         </div>
         <img src='/banner.png' alt='site-banner' style={{ maxWidth: '100%' }}></img>
+        <p>{title} - {description}</p>
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
