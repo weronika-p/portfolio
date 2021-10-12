00a23299176ad8af1784e3e4dc0f3eae13dca6b3
Weronika <weronika.pawlak1991@gmail.com>
2021-10-12 08:39:35 +0200

    feat(styles): Add styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
+import { Link } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
+import * as styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <section>
-        <div>Hello world!</div>
+      <section className={styles.header}>
+        <div>
+          <h2>Hi!</h2>
+          <h3>Welcome to my portfolio :)</h3>
+          <Link className={styles.btn} to='/projects'>My portfolio projects</Link>
+        </div>
       </section>
     </Layout>
   )
